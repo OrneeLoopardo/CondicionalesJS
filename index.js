4b6d7ce723e9b73b8d046f004a97dede65c23931
OrneeLoopardo <107761891+OrneeLoopardo@users.noreply.github.com>
2022-08-20 13:38:27 -0300

    Condicionales en JS

---
+++ index.js
@@ -0,0 +1,13 @@
+/*let number = parseInt(prompt("Ingrese un número"))
+
+if(number < 1000){
+    alert("¡Felicitaciones!")
+}else{
+    alert("Fallaste")
+}*/
+
+let text = "Ingrese un texto";
+if(text === "yo"){
+  console.log("entramos");
+}
+console.log("terminamos");
