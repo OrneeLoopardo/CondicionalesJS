7f636f7a3a49017476aa0617700002cdb88054f3
OrneeLoopardo <107761891+OrneeLoopardo@users.noreply.github.com>
2022-08-20 14:50:48 -0300

    cambio carpeta

--- Condicionales/index.js
+++ Condicionales/index.js
@@ -6,8 +6,15 @@ if(number < 1000){
     alert("Fallaste")
 }*/
 
-let text = "Ingrese un texto";
+/*let text = "Ingrese un texto";
 if(text === "yo"){
   console.log("entramos");
 }
 console.log("terminamos");
+*/
+
+/*let text = prompt ("")
+
+if (text == "hola"){
+    console.log("hola")
+}*/
