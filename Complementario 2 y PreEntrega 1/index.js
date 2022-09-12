10fcdcd18137ed512f94d0240ddbdc0f25e47a9e
OrneeLoopardo <107761891+OrneeLoopardo@users.noreply.github.com>
2022-09-11 23:27:58 -0300

    Muestro por alert adicional a la consola

--- Complementario 2 y PreEntrega 1/index.js
+++ Complementario 2 y PreEntrega 1/index.js
@@ -1,13 +1,36 @@
-const producto = [
+// POR CONSOLA
+/*const producto = [
     {nombre: 'plusbelle', valor: 150},
     {nombre: 'suave', valor: 250},
     {nombre: 'pantene', valor: 350},
     {nombre: 'sedal', valor: 500}
 ]
-const valor=producto.map((el)=>{
-    return{
-        nombre:el.nombre,
-        valor:el.valor + el.valor * 0.21 - el.valor * 0.5
+const valor = producto.map((el)=>{
+    return {
+        nombre : el.nombre,
+        valor : el.valor + el.valor * 0.21 - el.valor * 0.5
     }
 })
-console.log(valor)
+console.log(valor)*/
+
+// CON UN ALERT
+const productos = [
+    {nombre: 'plusbelle', valor: 150},
+    {nombre: 'suave', valor: 250},
+    {nombre: 'pantene', valor: 350},
+    {nombre: 'sedal', valor: 500}
+];
+
+const valor = productos.map((el)=>{
+    return{
+        nombre : el.nombre,
+        valor : el.valor + el.valor * 0.21 - (el.valor * 0.5)
+    }
+});
+
+let nombre = prompt("Ingrese el nombre del producto");
+let producto = valor.find(item => item.nombre == nombre);
+let precio = producto.valor;
+let mensaje = `El producto ${nombre} tiene un precio de ${precio}`;
+alert(mensaje);
+
