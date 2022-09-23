9dd89096fadb8fb725b5e613fdf1ebdcc9a48965
OrneeLoopardo <107761891+OrneeLoopardo@users.noreply.github.com>
2022-09-23 18:34:36 -0300

    Modificaciones de funcionalidad

---
+++ Proyecto/index.js
@@ -0,0 +1,19 @@
+function datos(){
+    var uno = document.getElementById("texto1").value;
+    var dos = document.getElementById("texto2").value;
+    var tres = document.getElementById("texto3").value;
+    var arreglo = [uno, dos, tres];
+    return arreglo;
+}
+const respuesta = () =>{
+    var arreglo = datos();
+    console.log(`su nombre es ${arreglo[0]}, su apellido ${arreglo[1]} y su edad ${arreglo[2]}`);
+};
+
+boton1.addEventListener("click", respuesta);
+/*
+function alerta(){
+    var arreglo = datos();
+    alert(`Su nombre es ${arreglo[0]}, su apellido ${arreglo[1]} y su edad ${arreglo[2]}`);
+}
+boton1.onclick = alerta;*/
