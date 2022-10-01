c5540f25f4cbe97f976eb962fa23402b2900ceed
OrneeLoopardo <107761891+OrneeLoopardo@users.noreply.github.com>
2022-10-01 17:46:04 -0300

    modifico formulario con modal, agrego paquetes, agrego imagenes y botones

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -1,34 +1,44 @@
-function datos(){
-    var uno = document.getElementById("texto1").value;
-    var dos = document.getElementById("texto2").value;
-    var tres = document.getElementById("texto3").value;
-    var arreglo = [uno, dos, tres];
-    return arreglo;
-}
+//MODAL PARA EL FORMULARIO
+const exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', event => {
+  // BOTON QUE ACTIVA EL MODAL
+  const button = event.relatedTarget
 
+  const recipient = button.getAttribute('data-bs-whatever')
+
+  const modalTitle = exampleModal.querySelector('.modal-title')
+  const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = `Contactate a ${recipient}`
+  modalBodyInput.value = recipient
+});
+
+//SWEET ALERT
+button2.addEventListener("click", () => {
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Sus datos han sido enviados",
+    showConfirmButton: false,
+    timer: 5000,
+  });
+});
+
+button1.addEventListener("click", () => {
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Sus datos han sido eliminados",
+    showConfirmButton: false,
+    timer: 5000,
+  });
+});
+
+//CONECTAR AL STORAGE
 const respuesta = () =>{
-const enJSON = JSON.stringify(new datos());
-
-localStorage.setItem("datos", enJSON);
-console.log(enJSON);
+  const enJSON = JSON.stringify(new datos());
+  
+  localStorage.setItem("datos", enJSON);
+  console.log(enJSON);
 };
-
-boton1.addEventListener("click", () => {
-    Swal.fire({
-      position: "center",
-      icon: "success",
-      title: "Sus datos han sido enviados",
-      showConfirmButton: false,
-      timer: 5000,
-    });
-});
-
-boton2.addEventListener("click", () => {
-    Swal.fire({
-      position: "center",
-      icon: "success",
-      title: "Sus datos han sido eliminados",
-      showConfirmButton: false,
-      timer: 5000,
-    });
-});
+  
